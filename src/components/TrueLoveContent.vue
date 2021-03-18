32b2196edf8c16ab82e8024064d295433d5d436e
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2021-03-18 19:22:15 +0300

    fix: delete useless attributes

--- src/components/TrueLoveContent.vue
+++ src/components/TrueLoveContent.vue
@@ -23,8 +23,6 @@
             class="story-video"
             :src="story.video"
             frameborder="0"
-            allow="accelerometer;
-            autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
         </div>
